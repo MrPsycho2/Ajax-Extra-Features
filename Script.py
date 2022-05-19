f951841c946fa89cd4368ee327ef875af9296736
MrPsycho2 <105348109+MrPsycho2@users.noreply.github.com>
2022-05-19 17:13:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -57,7 +57,7 @@ _𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐌𝐞 𝐖𝐢𝐭𝐡 𝐘𝐨𝐮𝐫 𝐂�
 
 ⪼ 𝐄𝐱𝐚𝐦𝐩𝐥𝐞 ›
 
-<code>/batch https://t.me/MalluHubGP/3 https://t.me/MWUpdatez/8</code>
+<code>/batch https://t.me/MalluHubGP/3 https://t.me/MalluHubGP/8</code>
 
 𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/MWUpdatez><b>𝙼𝚆-𝚄𝙿𝙳𝙰𝚃𝙴𝚉</b></a>"""
     WHOIS_TXT ="""<b>WHOIS MODULE</b>
